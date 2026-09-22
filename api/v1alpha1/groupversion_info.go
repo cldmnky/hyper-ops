95caa493ae73475fe5820150132fd2e168681d45
mbengtss <mbengtss@redhat.com>
2026-09-22 18:55:59 +0200

    Upgrade tooling and HyperShift API

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -20,17 +20,48 @@ limitations under the License.
 package v1alpha1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
 	// GroupVersion is group version used to register these objects
 	GroupVersion = schema.GroupVersion{Group: "hyper-ops.cloudmonkey.org", Version: "v1alpha1"}
 
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
+	//
+	// It is backed by the apimachinery scheme builders, following the pattern
+	// recommended by the deprecation notice of the controller-runtime
+	// scheme.Builder helper, so that this package only depends on apimachinery.
+	SchemeBuilder = &schemeBuilder{groupVersion: GroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+// schemeBuilder collects the API objects of one group version and applies them
+// to a runtime.Scheme when AddToScheme is called.
+type schemeBuilder struct {
+	groupVersion schema.GroupVersion
+	adders       []func(*runtime.Scheme) error
+}
+
+// Register adds the given API objects to the group version of the builder.
+func (b *schemeBuilder) Register(objects ...runtime.Object) {
+	b.adders = append(b.adders, func(s *runtime.Scheme) error {
+		s.AddKnownTypes(b.groupVersion, objects...)
+		metav1.AddToGroupVersion(s, b.groupVersion)
+		return nil
+	})
+}
+
+// AddToScheme applies all registered objects to the given scheme.
+func (b *schemeBuilder) AddToScheme(s *runtime.Scheme) error {
+	for _, add := range b.adders {
+		if err := add(s); err != nil {
+			return err
+		}
+	}
+	return nil
+}
